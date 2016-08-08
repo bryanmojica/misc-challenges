9a8b3698963174d8cecb266e86004d3b7c9fb2f2
bryanmojica <bryan@trestian.com>
2016-08-07 21:02:08 -0700

    Update w/ esLint adherence

--- letterObject.js
+++ letterObject.js
@@ -1,13 +1,12 @@
 
 // count occurences of char in string, passing the char as a key and the # occurences as the value
 var letterObject = function (string) {
-
-	var result = {};
-
-	for ( var i = 0; i < string.length; i ++ ) {
-		result[string[i]] ? result[string[i]] ++ : result[string[i]] = 1;
-	}
-
-	return result;
-
+  var result = {};
+  for ( var i = 0; i < string.length; i ++ ) {
+    result[string[i]] ? result[string[i]] ++ : result[string[i]] = 1;
+  }
+  return result;
 };
+
+
+console.log(letterObject('asjdiasjdiasodjasidjiasjdasd'));
